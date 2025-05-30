2a3413577a682c63656f20a92de3d5d8dbd08ddb
Gustavo <gustavo_fior@outlook.com>
2025-05-30 11:54:47 -0300

    lisa is back

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,13 +1,15 @@
+"use server";
+
 import Itzam from "itzam";
 import { env } from "~/env";
 
 const itzam = new Itzam(env.ITZAM_API_KEY);
 
-export const streamText = async (input: string) => {
+export async function sendMessageToLisa(input: string) {
   const response = await itzam.streamText({
     input,
     workflowSlug: "lisa",
   });
 
   return response;
-};
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import { cormorant } from "~/components/font";
 import { Input } from "~/components/input";
 import LisaOrb from "~/components/orb";
 import { TextEffect } from "~/components/text-effect";
+import { sendMessageToLisa } from "./actions";
 
 export default function HomePage() {
   const { bounce, triggerBounce } = useBounce();
@@ -27,54 +28,14 @@ export default function HomePage() {
     setIsLoading(true);
     setResponse("");
 
-    const res = await fetch("/api/generate-text", {
-      method: "POST",
-      body: JSON.stringify({ input }),
-    });
+    const response = await sendMessageToLisa(input);
 
-    const stream = res.body as ReadableStream;
-    const reader = stream.getReader();
-    const decoder = new TextDecoder();
-
-    try {
-      while (true) {
-        const { done, value } = (await reader.read()) as {
-          done: boolean;
-          value: Uint8Array;
-        };
-        if (done) {
-          break;
-        }
-
-        const chunk = decoder.decode(value);
-        const lines = chunk.split("\n");
-
-        for (let i = 0; i < lines.length; i++) {
-          const line = lines[i]?.trim() ?? "";
-          if (line.startsWith("event: ")) {
-            const eventType = line.slice(7);
-            // Skip the 'data: ' line
-            i++;
-            const data = lines[i]?.slice(6) ?? "";
-
-            if (eventType === "text") {
-              console.log("data", data);
-
-              // Append the text chunk to the output
-              setResponse((prev) => prev + data);
-            }
-          }
-        }
-      }
-    } catch (error) {
-      console.error("Error reading stream:", error);
-      throw error;
-    } finally {
-      setIsLoading(false);
-      setInput("");
-
-      reader.releaseLock();
+    for await (const chunk of response.stream) {
+      setResponse(chunk);
     }
+
+    setIsLoading(false);
+    setInput("");
   };
 
   return (
@@ -101,7 +62,7 @@ export default function HomePage() {
           </AnimatePresence>
         </div>
 
-        <p className={`${cormorant.className} text-stone-700`}>
+        <p className={`${cormorant.className} text-stone-500`}>
           Powered by{" "}
           <Link
             href="https://itz.am"
